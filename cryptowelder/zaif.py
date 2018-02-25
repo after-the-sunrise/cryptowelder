ee1a13124f9b5777efb7f818deef5cf94f614a8f
Takanori Takase <scm@after-sunrise.com>
2018-02-26 01:01:08 +0900

    Added zaif balance.

--- cryptowelder/zaif.py
+++ cryptowelder/zaif.py
@@ -1,7 +1,9 @@
-from threading import Thread
+from hashlib import sha512
+from hmac import new
+from threading import Thread, Lock
 from time import sleep
 
-from cryptowelder.context import CryptowelderContext, Ticker
+from cryptowelder.context import CryptowelderContext, Ticker, Balance, AccountType, UnitType
 
 
 class ZaifWelder:
@@ -11,6 +13,7 @@ class ZaifWelder:
         self.__context = context
         self.__logger = context.get_logger(self)
         self.__endpoint = self.__context.get_property(self._ID, 'endpoint', 'https://api.zaif.jp')
+        self.__lock = Lock()
         self.__thread = Thread(daemon=False, target=self._loop)
 
     def run(self):
@@ -30,6 +33,8 @@ class ZaifWelder:
             codes = self.__context.get_property(self._ID, 'codes', 'btc_jpy,bch_btc,eth_btc').split(',')
 
             threads = [Thread(target=self._process_ticker, args=(code,)) for code in codes]
+
+            threads.append(Thread(target=self._process_balance))
 
             for t in threads:
                 t.start()
@@ -67,6 +72,70 @@ class ZaifWelder:
         except Exception as e:
 
             self.__logger.warn('Ticker Failure - %s : %s - %s', code, type(e), e.args)
+
+    def _query_private(self, path):
+
+        apikey = self.__context.get_property(self._ID, 'apikey', None)
+        secret = self.__context.get_property(self._ID, 'secret', None)
+
+        if apikey is None or secret is None:
+            return None
+
+        with self.__lock:
+            sleep(0.001)  # Avoid duplicate nonce
+
+            time = self.__context.get_now().timestamp()
+
+            data = 'nonce=%.3f&method=%s' % (time, path)
+
+            digest = new(secret.encode(), data.encode(), sha512).hexdigest()
+
+            headers = {
+                "key": apikey,
+                "sign": digest
+            }
+
+            return self.__context.requests_post(self.__endpoint + '/tapi', headers=headers, data=data)
+
+    def _process_balance(self):
+
+        try:
+
+            response = self._query_private('get_info2')
+
+            if response is None or response.get('success', 1) != 1:
+                return
+
+            returns = response.get('return', {})
+
+            timestamp = self.__context.parse_iso_timestamp(returns.get('server_time'))
+
+            values = []
+
+            for currency, amount in returns.get('funds', {}).items():
+
+                try:
+                    unit = UnitType[currency.upper()]
+                except KeyError:
+                    continue
+
+                value = Balance()
+                value.bc_site = self._ID
+                value.bc_acct = AccountType.CASH
+                value.bc_unit = unit
+                value.bc_time = timestamp
+                value.bc_amnt = amount
+
+                values.append(value)
+
+            self.__context.save_balances(values)
+
+            for value in values:
+                self.__logger.debug('Cash : %s', value)
+
+        except Exception as e:
+
+            self.__logger.warn('Cash Failure : %s - %s', type(e), e.args)
 
 
 def main():
