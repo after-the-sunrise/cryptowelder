c5a38c6e1a1c203993cf6d522808e4024a30f4d2
Takanori Takase <scm@after-sunrise.com>
2018-04-15 01:01:12 +0900

    Fixed sleep for connection errors.

--- cryptowelder/context.py
+++ cryptowelder/context.py
@@ -191,16 +191,14 @@ class CryptowelderContext:
                             r.text.replace('\r', '').replace('\n', '') if r.text is not None else ''
                         )
 
-                sleep(float(self.get_property(self._SECTION, "request_sleep", 1.0)))
-
             except exceptions.RequestException as e:
 
                 self.__logger.debug('[%s][%s/%s] %s', type(e).__name__, count, attempt, label)
 
-                if count < attempt:
-                    continue
+                if count >= attempt:
+                    raise Exception(label) from e
 
-                raise Exception(label) from e
+            sleep(float(self.get_property(self._SECTION, "request_sleep", 1.0)))
 
     def requests_get(self, url, params=None, **kwargs):
 
